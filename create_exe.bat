70ae76029f043c7ee4fdbb2f365a56abcdd1150e
kavishka <ganewatthakavishka@gmail.com>
2023-03-23 02:54:42 +0530

    issue fixed

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,3 @@
 curl -o main.py https://raw.githubusercontent.com/KaViShKa99/file-encrypter/main/main.py
 pip install pyinstaller
-pyinstaller --onefile main.py
+pyinstaller --onefile --noconsole main.py
