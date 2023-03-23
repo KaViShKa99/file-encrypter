98c4c4f0c52d35cdf7320a47abde8189ae2c346d
kavishka <ganewatthakavishka@gmail.com>
2023-03-23 09:24:06 +0530

    bat file fixed

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,13 @@
+@echo off
 curl -o main.py https://raw.githubusercontent.com/KaViShKa99/file-encrypter/main/main.py
 pip install pyinstaller
 pyinstaller --name=KaviyaEncrypter --onefile -w main.py
+del /f /q main.py
+del /f /q main.spec
+rd /s /q build
+rd /s /q __pycache__
+cd dist
+copy KaviyaEncrypter.exe ..\KaviyaEncrypter.exe
+del /f /q KaviyaEncrypter.spec
+cd ..
+rmdir /s /q dist 
