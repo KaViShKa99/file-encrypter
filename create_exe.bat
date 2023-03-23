c37c5ae29071322480ac47923864396c26dc40b0
kavishka <ganewatthakavishka@gmail.com>
2023-03-23 16:23:33 +0530

    bat file fixed

--- create_exe.bat
+++ create_exe.bat
@@ -1,13 +1,30 @@
 @echo off
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 ( 
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe', 'python_installer.exe')"
+    python_installer.exe /quiet
+)
+
+
 curl -o main.py https://raw.githubusercontent.com/KaViShKa99/file-encrypter/main/main.py
+
 pip install pyinstaller
 pyinstaller --name=KaviyaEncrypter --onefile -w main.py
+
+
 del /f /q main.py
 del /f /q main.spec
+
+
 rd /s /q build
 rd /s /q __pycache__
+
+
 cd dist
-copy KaviyaEncrypter.exe ..\KaviyaEncrypter.exe
-del /f /q KaviyaEncrypter.spec
+copy KaviyaEncrypter.exe ..
 cd ..
-rmdir /s /q dist 
+
+
+del /f /q KaviyaEncrypter.spec
+rd /s /q dist
