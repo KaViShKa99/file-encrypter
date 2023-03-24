aa6861059681608e28cde81eab2c5b640d06f952
kavishka <ganewatthakavishka@gmail.com>
2023-03-25 02:29:05 +0530

    bat file fixed

--- create_exe.bat
+++ create_exe.bat
@@ -6,25 +6,20 @@ if %errorlevel% neq 0 (
     python_installer.exe /quiet
 )
 
-
 curl -o main.py https://raw.githubusercontent.com/KaViShKa99/file-encrypter/main/main.py
 
 pip install pyinstaller
-pyinstaller --name=KaviyaEncrypter --onefile -w main.py
-
+pyinstaller --name=KaviyaEncrypter --onefile -w main.py || echo There was an error during PyInstaller build process.
 
 del /f /q main.py
 del /f /q main.spec
 
-
 rd /s /q build
 rd /s /q __pycache__
-
 
 cd dist
 copy KaviyaEncrypter.exe ..
 cd ..
-
 
 del /f /q KaviyaEncrypter.spec
 rd /s /q dist
